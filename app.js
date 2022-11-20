6bb7b7764d4f1f8dd813866d15efec97b77adb76
theoneduke <104139400+theoneduke@users.noreply.github.com>
2022-11-20 03:58:38 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+let count = 0;
+
+let value = document.getElementById(`value`)
+let btns = document.querySelectorAll(`.btn`);
+
+console.log(value);
+btns.forEach(function (btn){
+    
+    btn.addEventListener(`click`, function(e){
+        const styles = e.currentTarget.classList
+        if (styles.contains(`decrease`)) {
+            count--;
+        } else if (styles.contains(`increase`)) {
+            count++;
+        } else {
+            count = 0;
+        }
+        if (count > 0) {
+            value.style.color = `green`;
+        }
+        if (count < 0) {
+            value.style.color = `red`;
+        }
+        if (count === 0) {
+            value.style.color = `#222`
+        }
+        value.textContent = count;
+    });
+});
